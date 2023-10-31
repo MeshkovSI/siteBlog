bd0720e3fc2d19e11a223c11ec8a04dba3a9b51c
Meshkov S. <stasmiddler@yandex.ru>
2023-10-31 21:50:07 +0300

    ready

---
+++ highcastBlog/highcastBlog/vievs.py
@@ -0,0 +1,19 @@
+from django.http import  HttpResponse
+from django.shortcuts import render
+
+MENU = {"HIGHCAST":"/", "Main":"/", "About":"/about"}
+
+def main_page(request):
+    title = "Main Page"
+    data = {"menu": MENU, "title": title}
+    return render(request, "./index.html", context=data)
+
+def post_page(request):
+    title = "Post"
+    data = {"menu": MENU, "title": title}
+    return render(request, "./post.html", context=data)
+
+def about_page(request):
+    title = "About"
+    data = {"menu": MENU, "title": title}
+    return render(request, "./about.html", context=data)
