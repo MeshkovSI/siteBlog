8df4caf7e4227425b0ec9cfb63f821d9d4d2dfc3
stasm <stasmiddler@yandex.ru>
2023-12-13 19:35:43 +0300

    13/12/2023

--- highcastBlog/catalog/views.py
+++ highcastBlog/catalog/views.py
@@ -6,15 +6,16 @@ MENU = {"HIGHCAST":"/", "Main":"/", "About":"/about"}
 
 def feedback_page(request):
     title = "Feedback"
-    data = {"menu": MENU, "title": title}
-    return render(request, "./feedback.html", context=data)
+    feedbacks = Feedback.objects.filter(checkbox=True)
+    data = {"menu": MENU, "title": title, "feedbacks": feedbacks}
+    return render(request, "feedback.html", context=data)
 
 def thanks_page(request):
     full_name = request.POST['full_name']
     email = request.POST['email']
     feedback = request.POST['feedback']
-    checkbox = request.POST['checkbox', False]
+    checkbox = request.POST.get('checkbox', False)
     Feedback.objects.create(full_name=full_name, email=email, feedback=feedback, checkbox=checkbox)
     title = "Thanks"
-    data = {"menu": MENU, "title": title, "full_name":full_name}
-    return render(request, "./thanks.html", context=data)
+    data = {"menu": MENU, "title": title, "full_name": full_name}
+    return render(request, "thanks.html", context=data)
